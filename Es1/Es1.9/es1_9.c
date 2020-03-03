ee860fb55f279ebaf960137b47e08539882b2146
mattiazingaretti <zingaretti.mattia@gmail.com>
2020-03-03 11:34:31 +0100

    ao

--- Es1/Es1.9/es1_9.c
+++ Es1/Es1.9/es1_9.c
@@ -1,1 +1,18 @@
-######sssavd###cadvodsanvoaudno
+#include<stdio.h>
+
+int main(){	
+	int c,entrato = 0 ;
+	printf("Star escape char\n");
+	while((c =getchar() != '*' && c != EOF){
+		while(c != '\n'){
+			if(c != ' '){
+				putchar(c);	
+			}else {while(c == ' '){;}
+				putchar(' ');}
+			}
+			c = getchar();
+		}
+	}
+
+	return 0;
+}
